09d4a04546da8ccb7697ade73c0cff61d98f9b01
marcin-rudnik-acoustic <32684308+Heisenburn@users.noreply.github.com>
2024-11-26 12:34:08 +0100

    fix import

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Game } from "./Components/Game/Game";
+import { Game } from "./components/Game/Game";
 import "./main.css";
 
 createRoot(document.getElementById("root")).render(
